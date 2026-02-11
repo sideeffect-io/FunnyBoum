3558b5db19b70874c27aa2d1f61b40d0232ab191
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2026-02-11 19:34:49 +0100

    Initial Commit

---
+++ FunnyBoom/FunnyBoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyBoomApp.swift
+//  FunnyBoom
+//
+//  Created by Thibault Wittemberg on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FunnyBoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FunnyBoomTests/FunnyBoomTests.swift
@@ -0,0 +1,16 @@
+//
+//  FunnyBoomTests.swift
+//  FunnyBoomTests
+//
+//  Created by Thibault Wittemberg on 11/02/2026.
+//
+
+import Testing
+
+struct FunnyBoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
